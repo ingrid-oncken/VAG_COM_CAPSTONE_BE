849fd147b69868fcb26afae8093592e5268a4fcc
Ingrid Oncken <ingrid.oncken@gmail.com>
2021-12-08 23:22:36 +0000

    q2m for products on get route

--- src/services/products/index.js
+++ src/services/products/index.js
@@ -9,7 +9,6 @@ const productsRouter = express.Router()
 //adminOnlyMiddleware,
 
 productsRouter.post('/', async (req, res, next) => {
-  console.log('Before try', req.body)
   try {
     const newProduct = new ProductModel(req.body)
     const { _id } = await newProduct.save()
@@ -25,6 +24,32 @@ productsRouter.post('/', async (req, res, next) => {
 })
 productsRouter.get('/', async (req, res, next) => {
   try {
+    const query = q2m(req.query)
+
+    console.log(query)
+
+    //countDocuments() will return to the frontend the total of documents GET is getting, total of items
+    //That is important fro the pagination, and by passing the query.criteria I retrive the filtered total
+    const total = await ProductModel.countDocuments(query.criteria)
+
+    //query.options.fields = PROJECTION
+
+    const products = await ProductModel.find(
+      query.criteria,
+      query.options.fields
+    )
+      .sort(query.options.sort)
+      .skip(query.options.skip)
+      .limit(query.options.limit)
+
+    //links: query.links('/products', total) is creating links for FE to use as prev/first/next/last in pagination
+    //pageTotal: Math.ceil(total / query.options.limit) is dividing the total of items for the stabilhed limit, also to be used by FE as info
+    res.send({
+      links: query.links('/products', total),
+      total,
+      products,
+      pageTotal: Math.ceil(total / query.options.limit),
+    })
   } catch (error) {
     next(error)
   }
