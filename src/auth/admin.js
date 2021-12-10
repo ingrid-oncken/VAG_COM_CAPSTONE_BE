33899ebadfcb463df1ea93518b15ebc05124f645
Ingrid Oncken <ingrid.oncken@gmail.com>
2021-12-10 11:45:34 +0000

    post products only admin done

--- src/auth/admin.js
+++ src/auth/admin.js
@@ -2,6 +2,7 @@ import createHttpError from 'http-errors'
 
 export const adminOnlyMiddleware = (req, res, next) => {
   if (req.user.role === 'Admin') {
+    //console.log('CLG req.user.role', req.user.role)
     next()
   } else {
     next(createHttpError(403, '403: Admins Only!'))
--- src/services/products/index.js
+++ src/services/products/index.js
@@ -2,59 +2,64 @@ import express from 'express'
 import ProductModel from './schema.js'
 import createHttpError from 'http-errors'
 import q2m from 'query-to-mongo'
-//import { adminOnlyMiddleware } from '../../auth/admin.js'
+import { JWTAuthMiddleware } from '../../auth/token.js'
+import { adminOnlyMiddleware } from '../../auth/admin.js'
 
 const productsRouter = express.Router()
 
 //adminOnlyMiddleware,
 
-productsRouter.post('/', async (req, res, next) => {
-  try {
-    const newProduct = new ProductModel(req.body)
-    const { _id } = await newProduct.save()
+productsRouter.post(
+  '/',
+  JWTAuthMiddleware,
+  adminOnlyMiddleware,
+  async (req, res, next) => {
+    try {
+      const newProduct = new ProductModel(req.body)
+      const { _id } = await newProduct.save()
 
-    res
-      .status(201)
-      .send(
-        `The new product ${newProduct.productName.toUpperCase()} was added with the ID: ${_id}`
-      )
-  } catch (error) {
-    // console.log(error)
-    next(error)
+      res
+        .status(201)
+        .send(
+          `The new product ${newProduct.productName.toUpperCase()} was added with the ID: ${_id}`
+        )
+    } catch (error) {
+      // console.log(error)
+      next(error)
+    }
   }
-})
+)
 productsRouter.get('/', async (req, res, next) => {
   try {
-    const products = await ProductModel.find()
-    console.log(products)
+    // const products = await ProductModel.find()
+    // console.log(products)
+    // res.send(products)
 
-    res.send(products)
+    const mongoQuery = q2m(req.query)
+    console.log(mongoQuery)
 
-    // const mongoQuery = q2m(req.query)
-    // console.log(mongoQuery)
+    //countDocuments() will return to the frontend the total of documents GET is getting, total of items
+    //That is important fro the pagination, and by passing the query.criteria I retrive the filtered total
 
-    // //countDocuments() will return to the frontend the total of documents GET is getting, total of items
-    // //That is important fro the pagination, and by passing the query.criteria I retrive the filtered total
+    //query.options.fields = PROJECTION
 
-    // //query.options.fields = PROJECTION
+    const total = await ProductModel.countDocuments(mongoQuery.criteria)
+    const products = await ProductModel.find(
+      mongoQuery.criteria
+      //mongoQuery.options.fields
+    )
+      .sort(mongoQuery.options.sort)
+      .skip(mongoQuery.options.skip)
+      .limit(mongoQuery.options.limit)
 
-    // const total = await ProductModel.countDocuments(mongoQuery.criteria)
-    // const products = await ProductModel.find(
-    //   mongoQuery.criteria,
-    //   //mongoQuery.options.fields
-    // )
-    //   .sort(mongoQuery.options.sort)
-    //   .skip(mongoQuery.options.skip)
-    //   .limit(mongoQuery.options.limit)
-
-    // //links: query.links('/products', total) is creating links for FE to use as prev/first/next/last in pagination
-    // //pageTotal: Math.ceil(total / query.options.limit) is dividing the total of items for the stabilhed limit, also to be used by FE as info
-    // res.send({
-    //   links: mongoQuery.links('/products', total),
-    //   pageTotal: Math.ceil(total / mongoQuery.options.limit),
-    //   total,
-    //   products,
-    // })
+    //links: query.links('/products', total) is creating links for FE to use as prev/first/next/last in pagination
+    //pageTotal: Math.ceil(total / query.options.limit) is dividing the total of items for the stabilhed limit, also to be used by FE as info
+    res.send({
+      links: mongoQuery.links('/products', total),
+      pageTotal: Math.ceil(total / mongoQuery.options.limit),
+      total,
+      products,
+    })
   } catch (error) {
     console.log(error)
     next(error)
@@ -63,7 +68,7 @@ productsRouter.get('/', async (req, res, next) => {
 productsRouter.get('/:id', async (req, res, next) => {
   try {
   } catch (error) {
-    next(error)
+    next(erro)
   }
 })
 productsRouter.get('/:id', async (req, res, next) => {
@@ -76,7 +81,7 @@ productsRouter.put('/:id', async (req, res, next) => {
   try {
   } catch (error) {
     next(error)
-  }
+ } 
 })
 
 export default productsRouter
