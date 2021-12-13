43e7db31bed11a689f1d0bc580154810115e02e5
Ingrid Oncken <ingrid.oncken@gmail.com>
2021-12-13 11:35:50 +0000

    ok

--- src/services/cart/index.js
+++ src/services/cart/index.js
@@ -9,110 +9,110 @@ cartsRouter.post(
   '/me/addProduct',
   JWTAuthMiddleware,
   async (req, res, next) => {
-    // try {
-    //   //retrive the product
-    //   const productId = req.body.productId
-    //   console.log('THIS IS productId cart/index.js', productId)
-
-    //   const purchasedProduct = await ProductModel.findById(productId)
-
-    //   const x = req.params.ownerId
-    //   console.log('THIS IS req.params.ownerId on cart/index.js', x)
-
-    //   if (purchasedProduct) {
-    //     //looking for the product using the ownerId AND the status
-    //     const isProductThere = await CartModel.findOne({
-    //       ownerId: req.params.ownerId,
-    //       status: 'active',
-    //       'products.productName': purchasedProduct.productName,
-    //     })
-
-    //     if (isProductThere) {
-    //       //if the product is already in the cart, only need to increase the quantity
-    //       const updatedCart = await CartModel.findOneAndUpdate(
-    //         {
-    //           ownerId: req.params.ownerId,
-    //           status: 'active',
-    //           'products.productName': purchasedProduct.productName,
-    //         },
-    //         {
-    //           $inc: {
-    //             'product.$.quantity': req.body.quantity,
-    //           },
-    //         }
-    //       )
-    //       res.send(updatedCart)
-    //     } else {
-    //       //so, if the product is not in the cart, let's add it
-    //       const productToInsert = {
-    //         ...purchasedProduct.toObject(),
-    //         quantity: req.body.quantity,
-    //       }
-
-    //       //upsert is a mongo property that create the cart is it does not exist yet
-    //       const updatedCart = await CartModel.findOneAndUpdate(
-    //         { ownerId: req.params.ownerId, status: 'active' },
-    //         {
-    //           $push: { products: productToInsert },
-    //         },
-    //         { runValidators: true, upsert: true, new: true }
-    //       )
-    //       res.send(updatedCart)
-    //     }
-    //   } else {
-    //     next(
-    //       createHttpError(
-    //         404,
-    //         `404: The product with the id ${productId} was not found`
-    //       )
-    //     )
-    //   }
-    // } catch (error) {
-    //   next(error)
-    // }
-
     try {
-      const { productId, quantity } = req.body
-      //console.log({ productId, quantity })
+      //retrive the product
+      const productId = req.body.productId
+      console.log('LINE 15 - THIS IS productId cart/index.js', productId)
 
       const purchasedProduct = await ProductModel.findById(productId)
-      //console.log(purchasedProduct)
+
+      const x = req.params.ownerId
+      console.log('LINE 20 - THIS IS req.params.ownerId on cart/index.js', x)
 
       if (purchasedProduct) {
+        //looking for the product using the ownerId AND the status
         const isProductThere = await CartModel.findOne({
           ownerId: req.params.ownerId,
           status: 'active',
+          'products.productName': purchasedProduct.productName,
         })
-        console.log(isProductThere.ownerId) //THIS IS RETURNING NULL
+
         if (isProductThere) {
-          const cart = await CartModel.findOneAndUpdate(
-            { ownerId: req.params.ownerId, status: 'active' },
-            { $inc: { 'products.$.quantity': quantity } },
-            { new: true }
+          //if the product is already in the cart, only need to increase the quantity
+          const updatedCart = await CartModel.findOneAndUpdate(
+            {
+              ownerId: req.params.ownerId,
+              status: 'active',
+              'products.productName': purchasedProduct.productName,
+            },
+            {
+              $inc: {
+                'product.$.quantity': req.body.quantity,
+              },
+            }
           )
-          res.send(cart)
+          res.send(updatedCart)
         } else {
-          const productToInsert = { ...purchasedProduct.toObject(), quantity }
+          //so, if the product is not in the cart, let's add it
+          const productToInsert = {
+            ...purchasedProduct.toObject(),
+            quantity: req.body.quantity,
+          }
 
-          const cart = await CartModel.findOneAndUpdate(
+          //upsert is a mongo property that create the cart is it does not exist yet
+          const updatedCart = await CartModel.findOneAndUpdate(
             { ownerId: req.params.ownerId, status: 'active' },
-            { $push: { products: productToInsert } },
-            { new: true, upsert: true }
+            {
+              $push: { products: productToInsert },
+            },
+            { runValidators: true, upsert: true, new: true }
           )
-
-          res.send(cart)
+          res.send(updatedCart)
         }
       } else {
         next(
           createHttpError(
             404,
-            `404: The product with the id ${productId} was not found.`
+            `404: The product with the id ${productId} was not found`
           )
         )
       }
     } catch (error) {
       next(error)
     }
+
+    // try {
+    //   const { productId, quantity } = req.body
+    //   console.log('Line 76', { productId, quantity })
+
+    //   const purchasedProduct = await ProductModel.findById(productId)
+    //   console.log('Line 79', purchasedProduct)
+
+    //   if (purchasedProduct) {
+    //     const isProductThere = await CartModel.findOne({
+    //       ownerId: req.params.ownerId,
+    //       status: 'active',
+    //     })
+    //     console.log('Line 86', isProductThere.ownerId) //THIS IS RETURNING NULL
+    //     if (isProductThere) {
+    //       const cart = await CartModel.findOneAndUpdate(
+    //         { ownerId: req.params.ownerId, status: 'active' },
+    //         { $inc: { 'products.$.quantity': quantity } },
+    //         { new: true }
+    //       )
+    //       res.send(cart)
+    //     } else {
+    //       const productToInsert = { ...purchasedProduct.toObject(), quantity }
+
+    //       const cart = await CartModel.findOneAndUpdate(
+    //         { ownerId: req.params.ownerId, status: 'active' },
+    //         { $push: { products: productToInsert } },
+    //         { new: true, upsert: true }
+    //       )
+
+    //       res.send(cart)
+    //     }
+    //   } else {
+    //     next(
+    //       createHttpError(
+    //         404,
+    //         `404: The product with the id ${productId} was not found.`
+    //       )
+    //     )
+    //   }
+    // } catch (error) {
+    //   next(error)
+    // }
   }
 )
 
